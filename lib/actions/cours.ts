702ad1f133070ed2dd0eb894cae994138ffbd0a1
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2026-03-19 15:14:04 +0100

    front-end composant de création de cours fini

--- lib/actions/cours.ts
+++ lib/actions/cours.ts
@@ -7,20 +7,13 @@ export async function creerCours(prevState: ActionResult, formData: FormData): P
     const startTime = formData.get("start-time");
     const duration = formData.get("duration");
     const groups = formData.getAll("groups");
-
-    console.log("Received form data:", { label, startDate, startTime, duration, groups });
-    // Simulate an API call to create a course
+    
     await new Promise((resolve) => setTimeout(resolve, 2000));
 
-
-    // For demonstration, we return a success result with the form data
     return {
         success: true,
         course: {
-            id: Math.floor(Math.random() * 1000), // Simulated course ID
-            name: formData.get("courseName"),
-            date: formData.get("courseDate"),
-            groups: formData.getAll("courseGroups"),
+            id: Math.floor(Math.random() * 1000)
         },
     };
 }
