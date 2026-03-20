d13353727325d7e8e8bd00ac6c6fe92fee6416d5
Baptiste Périaut <baptiste.periaut@etudiant.univ-rennes1.fr>
2026-03-20 11:07:18 +0100

    Création du querie course

---
+++ lib/db/queries/course.ts
@@ -0,0 +1,27 @@
+import { QueryModel } from './model'
+import * as lib from './lib'
+import { InferInsertModel, InferSelectModel } from 'drizzle-orm'
+
+const courseTable = lib.Schema.CourseTable.table
+
+type NewCourse = InferInsertModel<typeof courseTable>
+type Course = InferSelectModel<typeof courseTable>
+
+class CourseQueries extends QueryModel<NewCourse, Course> {
+    constructor() {
+        super(courseTable)
+    }
+
+    async getByStringId(stringId: string) {
+        const result = await lib.db
+            .select()
+            .from(this.table)
+            .where(lib.eq(this.table.courseId, stringId))
+        if (lib.resultEmpty(result)) {
+            return { error: "Cours introuvable avec cet ID." }
+        }
+        return { success: "Cours trouvé.", entity: result[0] as Course }
+    }
+}
+
+export const courseQueries = new CourseQueries()
