b3cbadd80132ddcbf3b81a2bcbddebb374aea33c
rcairou <raphael.cairou@gmail.com>
2026-03-20 11:59:34 +0100

    Recommencement de la migration

--- lib/db/migrations/0000_plain_rawhide_kid.sql
+++ lib/db/migrations/0000_plain_rawhide_kid.sql
@@ -5,6 +5,18 @@ CREATE TABLE IF NOT EXISTS "attendance" (
 	"student_mail" varchar(60) NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "course_group" (
+	"course_group_id" serial PRIMARY KEY NOT NULL,
+	"course_id" varchar(10) NOT NULL,
+	"group_id" integer NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "course_teacher" (
+	"course_teacher_id" serial PRIMARY KEY NOT NULL,
+	"course_id" varchar(10) NOT NULL,
+	"teacher_mail" varchar(60) NOT NULL
+);
+--> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "course" (
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
@@ -20,7 +32,7 @@ CREATE TABLE IF NOT EXISTS "group" (
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
 	"deleted_at" timestamp,
-	"groupId" serial PRIMARY KEY NOT NULL,
+	"group_id" serial PRIMARY KEY NOT NULL,
 	"promo" char(1) NOT NULL,
 	"td" char(1) NOT NULL,
 	"tp" char(1) NOT NULL,
@@ -34,7 +46,7 @@ CREATE TABLE IF NOT EXISTS "student" (
 	"last_name" varchar(30) NOT NULL,
 	"first_name" varchar(30) NOT NULL,
 	"password" varchar(100) NOT NULL,
-	"isTeacher" boolean DEFAULT false NOT NULL,
+	"is_teacher" boolean DEFAULT false NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
 	"deleted_at" timestamp,
@@ -47,7 +59,7 @@ CREATE TABLE IF NOT EXISTS "teacher" (
 	"last_name" varchar(30) NOT NULL,
 	"first_name" varchar(30) NOT NULL,
 	"password" varchar(100) NOT NULL,
-	"isTeacher" boolean DEFAULT false NOT NULL,
+	"is_teacher" boolean DEFAULT false NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
 	"deleted_at" timestamp
@@ -66,7 +78,31 @@ EXCEPTION
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "student" ADD CONSTRAINT "student_group_id_group_groupId_fk" FOREIGN KEY ("group_id") REFERENCES "public"."group"("groupId") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "course_group" ADD CONSTRAINT "course_group_course_id_course_course_id_fk" FOREIGN KEY ("course_id") REFERENCES "public"."course"("course_id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "course_group" ADD CONSTRAINT "course_group_group_id_group_group_id_fk" FOREIGN KEY ("group_id") REFERENCES "public"."group"("group_id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "course_teacher" ADD CONSTRAINT "course_teacher_course_id_course_course_id_fk" FOREIGN KEY ("course_id") REFERENCES "public"."course"("course_id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "course_teacher" ADD CONSTRAINT "course_teacher_teacher_mail_teacher_user_mail_fk" FOREIGN KEY ("teacher_mail") REFERENCES "public"."teacher"("user_mail") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "student" ADD CONSTRAINT "student_group_id_group_group_id_fk" FOREIGN KEY ("group_id") REFERENCES "public"."group"("group_id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
