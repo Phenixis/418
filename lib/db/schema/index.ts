631481c796d08839cafad32e6faa922e4b86974b
rcairou <raphael.cairou@gmail.com>
2026-03-23 08:32:39 +0100

    Ajout requêtes Cours-Groupe-Prof

--- lib/db/schema/index.ts
+++ lib/db/schema/index.ts
@@ -1,5 +1,7 @@
-export * as StudentTable from './student';
-export * as TeacherTable from './teacher';
-export * as GroupTable from './group';
-export * as CourseTable from './course';
-export * as AttendanceTable from './attendance';
+export * as StudentTable from './student'
+export * as TeacherTable from './teacher'
+export * as GroupTable from './group'
+export * as CourseTable from './course'
+export * as AttendanceTable from './attendance'
+export * as CourseGroupTable from './course-group'
+export * as CourseTeacherTable from './course-teacher'
