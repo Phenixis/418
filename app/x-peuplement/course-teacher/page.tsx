8d23353a93568402d2da9c51fc37775c69d31e17
Baptiste Périaut <baptiste.periaut@etudiant.univ-rennes1.fr>
2026-03-23 09:31:43 +0100

    Création de page des courses-teachers

---
+++ app/x-peuplement/course-teacher/page.tsx
@@ -0,0 +1,21 @@
+import { courseTeacherQueries } from "@/lib/db/queries/course-teacher"
+
+export default async function CourseTeacherPage() {
+    const courseTeachers = await courseTeacherQueries.getAll()
+    return (
+        <div>
+            <h1>Course Teachers</h1>
+            {
+                "success" in courseTeachers ? (
+                    <ul>
+                        {(courseTeachers.entity as any[]).map((courseTeacher) => (
+                            <li key={courseTeacher.id}>{JSON.stringify(courseTeacher, null, 2)}</li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>{courseTeachers.error}</p>
+                )
+            }
+        </div>
+    )
+}
