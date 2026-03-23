bba7c3a789debc1eda913108f2480b90f1024235
rcairou <raphael.cairou@gmail.com>
2026-03-23 10:39:51 +0100

    Suppression hours schema bdd

--- lib/db/schema/course.ts
+++ lib/db/schema/course.ts
@@ -4,7 +4,6 @@ import * as AttendanceTable from './attendance';
 export const table = lib.pgTable('course', {
     ...lib.baseAttributes,
     courseId: lib.varchar("course_id", { length: 36 }).primaryKey(),     // courseId est un UUID
-    hours: lib.time("hours").notNull(),
     startAt: lib.timestamp("start_at").notNull(),
     endAt: lib.timestamp("end_at").notNull(),
     subject: lib.varchar("subject", { length: 50 }).notNull()
