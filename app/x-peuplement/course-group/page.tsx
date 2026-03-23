8cb55b55d4d4aba528dafafb779cd9f2056555fa
Baptiste Périaut <baptiste.periaut@etudiant.univ-rennes1.fr>
2026-03-23 09:31:26 +0100

    Création de page des courses-groups

---
+++ app/x-peuplement/course-group/page.tsx
@@ -0,0 +1,21 @@
+import { courseGroupQueries } from "@/lib/db/queries/course-group"
+
+export default async function CourseGroupPage() {
+    const courseGroups = await courseGroupQueries.getAll()
+    return (
+        <div>
+            <h1>Course Groups</h1>
+            {
+                "success" in courseGroups ? (
+                    <ul>
+                        {(courseGroups.entity as any[]).map((courseGroup) => (
+                            <li key={courseGroup.id}>{JSON.stringify(courseGroup, null, 2)}</li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>{courseGroups.error}</p>
+                )
+            }
+        </div>
+    )
+}
