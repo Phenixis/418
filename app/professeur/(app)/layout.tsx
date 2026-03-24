e2906bf79cfd3c968904dc74c060fc01eeac6d20
bzbzz <nathan.thuault@protonmail.com>
2026-03-24 15:04:21 +0100

    pages dynamiques

--- app/professeur/(app)/layout.tsx
+++ app/professeur/(app)/layout.tsx
@@ -1,5 +1,7 @@
 import { Header } from '@/components/general/header';
 
+export const dynamic = "force-dynamic"
+
 export default function ProfesseurLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
