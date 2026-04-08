6f1a58f1fe5db8a3b975593e31a5baabed3823fd
bzbzz <nathan.thuault@protonmail.com>
2026-04-08 14:34:04 +0200

    bdd: ajout des champs iCal

--- lib/db/schema/teacher.ts
+++ lib/db/schema/teacher.ts
@@ -6,7 +6,8 @@ export const table = lib.pgTable('teacher', {
     ...lib.userAttributes,
     isAdmin: lib.boolean('is_admin').notNull().default(false),
     isValidated: lib.boolean('is_validated').notNull().default(false),
-    isFirstConnection: lib.boolean('is_first_connection').notNull().default(false)
+    isFirstConnection: lib.boolean('is_first_connection').notNull().default(false),
+    icalUrl: lib.varchar('ical_url', { length: 500 }),
 })
 
 export const relations = lib.relations(table, ({ many }) => ({
