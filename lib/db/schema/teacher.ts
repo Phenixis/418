c5c0696c601512ed946186f8955ab2a627e41f64
rcairou <raphael.cairou@gmail.com>
2026-04-08 11:10:58 +0200

    Ajout booleen first connection pour teacheer

--- lib/db/schema/teacher.ts
+++ lib/db/schema/teacher.ts
@@ -5,7 +5,8 @@ import * as SessionTeacherTable from './session-teacher';
 export const table = lib.pgTable('teacher', {
     ...lib.userAttributes,
     isAdmin: lib.boolean('is_admin').notNull().default(false),
-    isValidated: lib.boolean('is_validated').notNull().default(false)
+    isValidated: lib.boolean('is_validated').notNull().default(false),
+    isFirstConnection: lib.boolean('is_fist_connection').notNull().default(false)
 })
 
 export const relations = lib.relations(table, ({ many }) => ({
