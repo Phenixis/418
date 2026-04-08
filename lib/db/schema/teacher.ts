3980489f452b8d9215c3885093a392df341a91f5
rcairou <raphael.cairou@gmail.com>
2026-04-08 11:17:31 +0200

    Fistiland au fond du fun

--- lib/db/schema/teacher.ts
+++ lib/db/schema/teacher.ts
@@ -6,7 +6,7 @@ export const table = lib.pgTable('teacher', {
     ...lib.userAttributes,
     isAdmin: lib.boolean('is_admin').notNull().default(false),
     isValidated: lib.boolean('is_validated').notNull().default(false),
-    isFirstConnection: lib.boolean('is_fist_connection').notNull().default(false)
+    isFirstConnection: lib.boolean('is_first_connection').notNull().default(false)
 })
 
 export const relations = lib.relations(table, ({ many }) => ({
